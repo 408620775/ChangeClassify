3cad228db34b0a82251800925977d292a8455a0b
408620775 <408620775@qq.com>
2017-01-19 11:15:53 +0800

    add something

--- src/pers/bbn/changeBug/extraction/Extraction.java
+++ src/pers/bbn/changeBug/extraction/Extraction.java
@@ -27,8 +27,8 @@ import java.util.Map;
  *
  */
 public abstract class Extraction {
-    String sql;
-    Statement stmt;
+	String sql;
+	Statement stmt;
 	ResultSet resultSet;
 	int start;
 	int end;
@@ -42,12 +42,12 @@ public abstract class Extraction {
 	 * extraction2提取信息并不需要miningit生成的数据，此构造函数只是为了统一接口。
 	 * 
 	 * @param database
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public Extraction(String database,int start,int end) throws Exception  { // 为extraction2提供构造函数。	
-		this.start=start;
-		this.end=end;
-		this.databaseName=database;
+	public Extraction(String database, int start, int end) throws Exception { // 为extraction2提供构造函数。
+		this.start = start;
+		this.end = end;
+		this.databaseName = database;
 		this.sqlL = new SQLConnection(database);
 		this.stmt = sqlL.getStmt();
 		initialCommitFileIds();
@@ -55,29 +55,33 @@ public abstract class Extraction {
 
 	/**
 	 * 根据给定的范围获取有效的commit_file对,也就是包括删除在内的记录对.
+	 * 
 	 * @throws Exception
 	 */
 	private void initialCommitFileIds() throws Exception {
-		commit_ids=new ArrayList<>();
-		commitIdPart=new ArrayList<>();
+		commit_ids = new ArrayList<>();
+		commitIdPart = new ArrayList<>();
 		sql = "select id from scmlog order by commit_date";
 		resultSet = stmt.executeQuery(sql);
 		while (resultSet.next()) {
 			commit_ids.add(resultSet.getInt(1));
 		}
-		if (start<0) {
+		if (start < 0) {
 			throw new Exception("start can't be less 0!");
 		}
-		if (end>commit_ids.size()) {
-			throw new Exception("end is larger than the total number of commits!");
+		if (end > commit_ids.size()) {
+			throw new Exception(
+					"end is larger than the total number of commits!");
 		}
-		for (int i = start-1; i < end; i++) {
-			sql="select file_id,current_file_path from actions where commit_id="+commit_ids.get(i)+" and type!='D'";
-			resultSet=stmt.executeQuery(sql);
+		for (int i = start - 1; i < end; i++) {
+			sql = "select file_id,current_file_path from actions where commit_id="
+					+ commit_ids.get(i) + " and type!='D'";
+			resultSet = stmt.executeQuery(sql);
 			while (resultSet.next()) {
-				String current_file_path=resultSet.getString(2);
-				if (current_file_path.endsWith(".java")&&(!current_file_path.toLowerCase().contains("test"))) {
-					List<Integer> tmp=new ArrayList<>();
+				String current_file_path = resultSet.getString(2);
+				if (current_file_path.endsWith(".java")
+						&& (!current_file_path.toLowerCase().contains("test"))) {
+					List<Integer> tmp = new ArrayList<>();
 					tmp.add(commit_ids.get(i));
 					tmp.add(resultSet.getInt(1));
 					commit_fileIds.add(tmp);
@@ -97,12 +101,13 @@ public abstract class Extraction {
 	}
 
 	/**
-	 * 根据给定的一组commit_fileId对,获取其对应的内容.
-	 * 需要注意的是stringBuffer格式的content后面以逗号结尾
+	 * 根据给定的一组commit_fileId对,获取其对应的内容. 需要注意的是stringBuffer格式的content后面以逗号结尾
+	 * 
 	 * @param commit_fileIds
 	 * @return
 	 * @throws SQLException
 	 */
-	public abstract Map<List<Integer>, StringBuffer> getContentMap(List<List<Integer>> someCommit_fileIds) throws SQLException;
+	public abstract Map<List<Integer>, StringBuffer> getContentMap(
+			List<List<Integer>> someCommit_fileIds) throws SQLException;
 
 }
